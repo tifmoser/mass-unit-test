eeb0d8ce0898349e5657c73a76719e27cfcce556
tifmoser <72584361+tifmoser@users.noreply.github.com>
2020-10-08 23:15:33 -0400

    Implemented default arguments
    Combined functions a and b into one using the power of default arguments. Twiddled with the code a bit to make this work properly.

--- mass_unit_test.py
+++ mass_unit_test.py
@@ -1,32 +1,10 @@
-def mass_unit_test_a (function_name: str, inputs: str, split_char: str):
-    '''
-    This function generates unit tests for the given function, when it's called with
-    the given inputs. These unit tests can be pasted into your code, as long as the
-    assert_equal() function is imported from the cisc108 library. Just don't forget
-    to type in the outputs afterwards!
-    
-    Args:
-        function_name (str): The name of the function being unit tested
-        inputs (str): A triple-quoted string containing all the testing inputs, each
-            separated by line breaks
-    
-    Returns:
-        (str): Unit tests that can be copy+pasted directly into your code, with blanks
-            where the desired outputs should be specified
-    '''
-    x = inputs.split(split_char)
-    for i in x:
-        print("assert_equal(" + function_name + "(" + i + "), ___)")
-    return
-
-
-
-def mass_unit_test_b (function_name: str, inputs: str, split_char_in:str, outputs: str, split_char_out: str):
+def mass_unit_test (function_name: str, inputs: str, split_char_in:str, outputs='', split_char_out='3Xkmb4Qk'):
     '''
     This function generates unit tests to test the given function for the given
     outputs, when it's called with the given inputs. These unit tests can be pasted
     into your code, as long as the assert_equal() function is imported from the
-    cisc108 library.
+    cisc108 library. If outputs are not specified, they will be replaced with a blank
+    (___).
     
     Args:
         function_name (str): The name of the function being unit tested
@@ -39,7 +17,12 @@ def mass_unit_test_b (function_name: str, inputs: str, split_char_in:str, output
         (str): Unit tests that can be copy+pasted directly into your code
     '''
     x = inputs.split(split_char_in)
-    y = outputs.split(split_char_out)
+    y = []
+    if outputs:
+        y = outputs.split(split_char_out)
+    else:
+        for i in inputs:
+            y.append("___")
     if len(x) == len(y):
         for i in range(len(x)):
             print("assert_equal(" + function_name + "(" + x[i] + "), " + y[i] + ")")
