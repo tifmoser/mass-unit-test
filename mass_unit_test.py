9800b698a0b10246c08347716ce3e411a30707db
tifmoser <72584361+tifmoser@users.noreply.github.com>
2020-10-08 20:40:19 -0400

    Added initial files

---
+++ mass_unit_test.py
@@ -0,0 +1,49 @@
+def mass_unit_test_a (function_name: str, inputs: str):
+    '''
+    This function generates unit tests for the given function, when it's called with
+    the given inputs. These unit tests can be pasted into your code, as long as the
+    assert_equal() function is imported from the cisc108 library. Just don't forget
+    to type in the outputs afterwards!
+    
+    Args:
+        function_name (str): The name of the function being unit tested
+        inputs (str): A triple-quoted string containing all the testing inputs, each
+            separated by line breaks
+    
+    Returns:
+        (str): Unit tests that can be copy+pasted directly into your code, with blanks
+            where the desired outputs should be specified
+    '''
+    x = inputs.split('\n')
+    for i in x:
+        print("assert_equal(" + function_name + "(" + i + "), ___)")
+    return
+
+
+
+def mass_unit_test_b (function_name: str, inputs: str, outputs: str):
+    '''
+    This function generates unit tests to test the given function for the given
+    outputs, when it's called with the given inputs. These unit tests can be pasted
+    into your code, as long as the assert_equal() function is imported from the
+    cisc108 library.
+    
+    Args:
+        function_name (str): The name of the function being unit tested
+        inputs (str): A triple-quoted string containing all the tested inputs, each
+            separated by line breaks
+        outputs (str): A triple-quoted string containing all the tested outputs, each
+            separated by line breaks. Must line up one-to-one with inputs.
+    
+    Returns:
+        (str): Unit tests that can be copy+pasted directly into your code
+    '''
+    x = inputs.split('\n')
+    y = outputs.split('\n')
+    if len(x) == len(y):
+        for i in range(len(x)):
+            print("assert_equal(" + function_name + "(" + x[i] + "), " + y[i] + ")")
+    else:
+        print("FAILED: inputs and outputs don't line up")
+    return
+        
